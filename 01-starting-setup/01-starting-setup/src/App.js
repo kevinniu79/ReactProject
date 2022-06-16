a80e0feae49efcbc25b97358f73bee619ca6daf7
Sirat Mokha <mokha1671@gmail.com>
2022-06-16 12:33:11 -0500

    additional expense item changes

--- 01-starting-setup/01-starting-setup/src/App.js
+++ 01-starting-setup/01-starting-setup/src/App.js
@@ -1,8 +1,12 @@
+import React from "react";
+import ExpenseItem from "./Components/ExpenseItem";
+
 function App() {
   return (
     <div>
       <h2>Let's get started!</h2>
       <p>This is also visible.</p>
+      <ExpenseItem></ExpenseItem>
     </div>
   );
 }
