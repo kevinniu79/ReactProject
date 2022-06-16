98efb95d589347bcfb389c38b9fba53606008c12
Jason <jasondong03@gmail.com>
2022-06-16 13:25:15 -0500

    Making HTML more dynamic

--- 01-starting-setup/01-starting-setup/src/components/ExpenseItem.js
+++ 01-starting-setup/01-starting-setup/src/components/ExpenseItem.js
@@ -2,12 +2,17 @@ import React from 'react';
 import "./ExpenseItem.css";
 
 function ExpenseItem() {
+    
+    const expenseDate = new Date(2021, 2, 28);
+    const expenseTitle = 'Car Insurance';
+    const expenseAmount = 294.67;
+
     return (
-        <div className = 'expense-item'>
-            <div>June 10th 2022</div>
-            <div className = 'expense-item__description'>
-                <h2>Car Insruance</h2>
-                <div className='expense-item__price'>$294.67</div>
+        <div className='expense-item'>
+            <div> {expenseDate.toISOString()}</div>
+            <div className='expense-item__description'>
+                <h2>{expenseTitle}</h2>
+                <div className= 'expense-item_price'>${expenseAmount}</div>
             </div>
         </div>
     )
