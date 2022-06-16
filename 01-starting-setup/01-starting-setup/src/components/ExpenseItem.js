6f8f00a53c64e707f967955a3c5916f3a0f362eb
Kevin Niu <kevin.h.niu@gmail.com>
2022-06-16 14:11:13 -0500

    Fixing small bugs

--- 01-starting-setup/01-starting-setup/src/components/ExpenseItem.js
+++ 01-starting-setup/01-starting-setup/src/components/ExpenseItem.js
@@ -6,7 +6,7 @@ function ExpenseItem(props) {
     <div className="expense-item">
       <div>{props.date.toISOString()}</div>
       <div className='expense-item__description'>
-        <h2>{props.titlee}</h2>
+        <h2>{props.title}</h2>
         <div className='expense-item__price'>${props.amount}</div>
       </div>
     </div>
