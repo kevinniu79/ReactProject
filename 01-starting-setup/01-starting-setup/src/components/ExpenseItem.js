e2f9b84c400ad22a93da1d17268dcd5e222dfe11
Holly R <hraetz14@gmail.com>
2022-06-16 14:30:25 -0500

    date change

--- 01-starting-setup/01-starting-setup/src/components/ExpenseItem.js
+++ 01-starting-setup/01-starting-setup/src/components/ExpenseItem.js
@@ -7,8 +7,8 @@ function ExpenseItem(props) {
     return (
         <div className='expense-item'>
             <div>{month}</div>
+            <div>{day}, </div>
             <div>{year}</div>
-            <div>{day}|</div>
             <div className='expense-item__description'>
                 <h2>{props.title}</h2>
                 <div className='expense-item__price'>${props.amount}</div>
