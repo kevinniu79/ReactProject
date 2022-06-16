fa4f752b271c451ab0e6b6e2772cb46068e3724e
Aaditya Tiwari (BCHS) <24aaditf@gmail.com>
2022-06-16 12:56:19 -0500

    Expense Item css and js

--- 01-starting-setup/01-starting-setup/src/components/ExpenseItem.js
+++ 01-starting-setup/01-starting-setup/src/components/ExpenseItem.js
@@ -1,9 +1,14 @@
 import React from 'react';
-function ExpenseItem(){
+import "./ExpenseItem.css";
+function ExpenseItem() {
 
     return (
-        <div>
-            <h2>Expense Item!</h2>
+        <div className='expense-item'>
+            <div>June 10th 2022</div>
+            <div className='expense-item__description'>
+                <h2>Car Insurance</h2>
+                <div className='expense-item__price'>$294.67</div>
+            </div>
         </div>
     )
 
