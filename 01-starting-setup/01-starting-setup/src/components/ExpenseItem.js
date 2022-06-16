9967e4233149aa1ead41924235de01d1fb11894d
Noah Ren <nren2025@gmail.com>
2022-06-16 14:54:34 -0500

    LocaleString
    Updated

--- 01-starting-setup/01-starting-setup/src/components/ExpenseItem.js
+++ 01-starting-setup/01-starting-setup/src/components/ExpenseItem.js
@@ -4,11 +4,11 @@ function ExpenseItem(props) {
 
     const month = props.date.toLocaleString('en-US', {month: 'long'});
     const day = props.date.toLocaleString('en-US', {day: '2-digit'});
-    const year = props.data.getFullYear();
+    const year = props.date.getFullYear();
   return (
-    <div className="expense-item">
-      <div>{month}</div>
-      <div>{year}</div>
+    <div className='expense-item'>
+      <div>{month}, </div>
+      <div>{year}, </div>
       <div>{day}</div>
       <div className='expense-item__description'>
         <h2>{props.title}</h2>
