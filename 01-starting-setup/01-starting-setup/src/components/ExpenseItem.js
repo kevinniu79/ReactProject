a81b4c679251a5b86a64be83c8c6275290ab4c35
Kevin Niu <kevin.h.niu@gmail.com>
2022-06-16 14:33:37 -0500

    switch day and year for the date

--- 01-starting-setup/01-starting-setup/src/components/ExpenseItem.js
+++ 01-starting-setup/01-starting-setup/src/components/ExpenseItem.js
@@ -7,9 +7,9 @@ function ExpenseItem(props) {
     const year = props.date.getFullYear();
   return (
     <div className="expense-item">
-      <div>{month}, </div>
-      <div>{year}, </div>
+      <div>{month}</div>
       <div>{day}</div>
+      <div>{year}</div>
       <div className='expense-item__description'>
         <h2>{props.title}</h2>
         <div className='expense-item__price'>${props.amount}</div>
