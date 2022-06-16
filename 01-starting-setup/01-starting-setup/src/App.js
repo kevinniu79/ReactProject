10fe3dc435b308f7063693dd6c677885a7693d5b
Holly R <hraetz14@gmail.com>
2022-06-16 12:56:03 -0500

    Expense Item css

--- 01-starting-setup/01-starting-setup/src/App.js
+++ 01-starting-setup/01-starting-setup/src/App.js
@@ -1,9 +1,11 @@
-Import React from 'react';
+import React from 'react';
+import ExpenseItem from './components/ExpenseItem.js';
 function App() {
   return (
     <div>
       <h2>Let's get started!</h2>
       <p>this is visible text!!</p>
+      <ExpenseItem></ExpenseItem>
     </div>
   );
 }
--- 01-starting-setup/01-starting-setup/src/components/ExpenseItem.js
+++ 01-starting-setup/01-starting-setup/src/components/ExpenseItem.js
@@ -1,11 +1,15 @@
 import React from 'react';
+import "./ExpenseItem.css";
 function ExpenseItem(){
     return(
 
-        <div>
-            <h2>Expense Item</h2>
+        <div className = 'expense-item'>
+            <div>june 10 2022</div>
+            <div className = 'expense-item__description'>
+                <h2> car insurance</h2>
+                <div className='expense-item__price'>$3</div>
+            </div>
         </div>
-
     )
 }
 export default ExpenseItem;
