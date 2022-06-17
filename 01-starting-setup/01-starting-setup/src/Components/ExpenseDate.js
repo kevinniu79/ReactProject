2d94b6b9c87c3ec120786f40a1a9243b772bbe40
Sirat Mokha <mokha1671@gmail.com>
2022-06-17 09:39:36 -0500

    Expense Date added

--- 01-starting-setup/01-starting-setup/src/Components/ExpenseDate.js
+++ 01-starting-setup/01-starting-setup/src/Components/ExpenseDate.js
@@ -1,5 +1,5 @@
-import react from "react";
-
+import React from "react";
+import './ExpenseDate.css'; 
 function ExpenseDate(props){
 
     const month = props.date.toLocaleString('en-US', {month: 'long'}); 
@@ -7,10 +7,11 @@ function ExpenseDate(props){
     const year = props.date.getFullYear(); 
 
     return(
-        <div>
- <div>{month}, </div>
-            <div>{year}, </div>
-            <div>{day}</div>
+        <div className="expense-date">
+    
+            <div className="expense-date__month">{month}</div>
+            <div className="expense-date__year">{year }</div>
+            <div className="expense-date__day">{day}</div>
 
         </div>
     )
--- 01-starting-setup/01-starting-setup/src/Components/ExpenseItem.js
+++ 01-starting-setup/01-starting-setup/src/Components/ExpenseItem.js
@@ -1,12 +1,13 @@
 import React from 'react'; 
 import "./ExpenseItem.css"; 
+import ExpenseDate from './ExpenseDate';
 function ExpenseItem(props){
 
    
 
     return(
         <div className='expense-item'>
-           
+           <ExpenseDate date={props.date}></ExpenseDate>
             <div className='expense-item__description'>
                 <h2>{props.title}</h2>
                 <div className='expense-item__price'>${props.amount}</div>
