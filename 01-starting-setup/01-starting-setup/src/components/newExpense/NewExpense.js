f62f744f91ee778de0bdedb4df301f504eef67e6
Aaditya Tiwari (BCHS) <24aaditf@gmail.com>
2022-06-20 12:18:45 -0500

    Using form data

--- 01-starting-setup/01-starting-setup/src/components/newExpense/NewExpense.js
+++ 01-starting-setup/01-starting-setup/src/components/newExpense/NewExpense.js
@@ -4,9 +4,17 @@ import ExpenseForm from './ExpenseForm';
 
 function NewExpense(){
 
+    function saveExpenseDataHandler(enteredExpenseData){
+
+        const expenseData = {
+            ...enteredExpenseData,
+            id:Math.random().toString()
+        };
+        console.log(expenseData);
+    }
     return(
         <div className='new-expense'>
-            <ExpenseForm></ExpenseForm>
+            <ExpenseForm onSaveExpenseData={saveExpenseDataHandler}></ExpenseForm>
         </div>
 
     )
