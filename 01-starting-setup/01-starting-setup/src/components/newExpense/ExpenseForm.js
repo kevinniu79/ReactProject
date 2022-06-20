ba8b126d7dfdf5c06b584bde3a431df5860e9c83
Kevin Niu <kevin.h.niu@gmail.com>
2022-06-20 12:18:35 -0500

    Using form data

--- 01-starting-setup/01-starting-setup/src/components/newExpense/ExpenseForm.js
+++ 01-starting-setup/01-starting-setup/src/components/newExpense/ExpenseForm.js
@@ -1,7 +1,7 @@
 import React, {useState} from 'react';
 import './ExpenseForm.css';
 
-function ExpenseForm() {
+function ExpenseForm(props) {
 
     const [enteredTitle, setEnteredTitle] = useState("");
     const [enteredAmount, setEnteredAmount] = useState("");
@@ -26,6 +26,7 @@ function ExpenseForm() {
             amount: +enteredAmount,
             date: new Date(enteredDate),
         };
+        props.onSaveExpenseData(expenseData);
         console.log(expenseData);
         setEnteredAmount("");
         setEnteredDate("");
