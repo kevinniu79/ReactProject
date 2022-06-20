fd3aabe22cb230bc47ef14dd8912048eec4635a0
Noah Ren <nren2025@gmail.com>
2022-06-20 14:12:15 -0500

    Dropdown Functional
    Dropdown now works

--- 01-starting-setup/01-starting-setup/src/components/Expenses.js
+++ 01-starting-setup/01-starting-setup/src/components/Expenses.js
@@ -6,6 +6,9 @@ import ExpenseFilter from "./newExpense/ExpensesFilter.js";
 function Expenses(props) {
 
   const [filteredYear, setFilteredYear] = useState("2020");
+  const filteredExpenses = props.items.filter(expenses =>{
+    return expenses.date.getFullYear().toString() === filteredYear;
+  });
 
   function filterChangeHandler(selectedYear){
     setFilteredYear(selectedYear);
@@ -14,7 +17,7 @@ function Expenses(props) {
   return (
     <Card className="expenses">
       <ExpenseFilter selected={filteredYear} onFilterChange={filterChangeHandler}></ExpenseFilter>
-      {props.items.map((expensesElement) => (
+      {filteredExpenses.map((expensesElement) => (
         <ExpenseItem
         key={expensesElement.id}
           title={expensesElement.title}
