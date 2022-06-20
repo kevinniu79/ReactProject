0c4fa8325018b05282e755c5cd7651da5af39271
Noah Ren <nren2025@gmail.com>
2022-06-20 10:14:11 -0500

    Added NewExpense
    Added

--- 01-starting-setup/01-starting-setup/src/App.js
+++ 01-starting-setup/01-starting-setup/src/App.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import Expenses from './components/Expenses.js';
+import NewExpense from './components/newExpense/NewExpense.js';
 function App() {
   const expenses = [
     {
@@ -29,6 +30,7 @@ function App() {
   ]
   return (
       <div>
+        <NewExpense></NewExpense>
               <h2>Let's get started!</h2>
         <Expenses items={expenses}></Expenses>
       </div>
---
+++ 01-starting-setup/01-starting-setup/src/components/newExpense/NewExpense.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './NewExpense.css';
+import ExpenseForm from './ExpenseForm.js';
+function NewExpense() {
+
+    return(
+        <div className='new-expense'>
+            <ExpenseForm></ExpenseForm>
+            </div>
+    )
+
+}
+
+export default NewExpense;
