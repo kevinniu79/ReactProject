13098d1e785d7d4a26a829c8f250150a4082deb7
Kevin Niu <kevin.h.niu@gmail.com>
2022-06-20 13:40:30 -0500

    making initial array fluid

--- 01-starting-setup/01-starting-setup/src/components/Expenses.js
+++ 01-starting-setup/01-starting-setup/src/components/Expenses.js
@@ -12,27 +12,18 @@ function Expenses(props) {
 
   return (
     <Card className="expenses">
-      <ExpenseFilter selected={filteredYear} onFilterChange={filterChangeHandler}></ExpenseFilter>
-      <ExpenseItem
-        title={props.items[0].title}
-        amount={props.items[0].amount}
-        date={props.items[0].date}
-      ></ExpenseItem>
-      <ExpenseItem
-        title={props.items[1].title}
-        amount={props.items[1].amount}
-        date={props.items[1].date}
-      ></ExpenseItem>
-      <ExpenseItem
-        title={props.items[2].title}
-        amount={props.items[2].amount}
-        date={props.items[2].date}
-      ></ExpenseItem>
-      <ExpenseItem
-        title={props.items[3].title}
-        amount={props.items[3].amount}
-        date={props.items[3].date}
-      ></ExpenseItem>
+      <ExpenseFilter
+        selected={filteredYear}
+        onFilterChange={filterChangeHandler}
+      ></ExpenseFilter>
+      
+      {props.items.map((expensesElement) => (
+        <ExpenseItem
+          title={expensesElement.title}
+          amount={expensesElement.amount}
+          date={expensesElement.date}
+        ></ExpenseItem>
+      ))}
     </Card>
   );
 }
