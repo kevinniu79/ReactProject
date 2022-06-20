20640e958382461d24c1382f3f2ec90300d1094d
Aaditya Tiwari (BCHS) <24aaditf@gmail.com>
2022-06-20 13:40:36 -0500

    Making initial array field

--- 01-starting-setup/01-starting-setup/src/components/Expenses.js
+++ 01-starting-setup/01-starting-setup/src/components/Expenses.js
@@ -1,4 +1,4 @@
-import React, {useState} from "react";
+import React, { useState } from "react";
 import './Expenses.css';
 import ExpenseItem from "./ExpenseItem";
 import Card from './Card.js';
@@ -8,33 +8,20 @@ function Expenses(props) {
 
     const [filteredYear, setFilteredYear] = useState("2020");
 
-    function filterChangeHandler(selectedYear){
+    function filterChangeHandler(selectedYear) {
         setFilteredYear(selectedYear);
     }
 
     return (
         <Card className="expenses">
             <ExpensesFilter selected={filteredYear} onFilterChange={filterChangeHandler}></ExpensesFilter>
-            <ExpenseItem
-                title={props.items[0].title}
-                amount={props.items[0].amount}
-                date={props.items[0].date}
-            ></ExpenseItem>
-            <ExpenseItem
-                title={props.items[1].title}
-                amount={props.items[1].amount}
-                date={props.items[1].date}
-            ></ExpenseItem>
-            <ExpenseItem
-                title={props.items[2].title}
-                amount={props.items[2].amount}
-                date={props.items[2].date}
-            ></ExpenseItem>
-            <ExpenseItem
-                title={props.items[3].title}
-                amount={props.items[3].amount}
-                date={props.items[3].date}
-            ></ExpenseItem>
+            {props.items.map((expensesElement) => (
+                <ExpenseItem
+                    title={expensesElement.title}
+                    amount={expensesElement.amount}
+                    date={expensesElement.date}
+                ></ExpenseItem>
+            ))}
         </Card>
     );
 
