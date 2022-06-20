12a5ac91110d9527f6a840e09e3263f8e17d7e85
Sirat Mokha <mokha1671@gmail.com>
2022-06-20 14:12:04 -0500

    Functional Dropdown

--- 01-starting-setup/01-starting-setup/src/Components/Expenses.js
+++ 01-starting-setup/01-starting-setup/src/Components/Expenses.js
@@ -6,7 +6,13 @@ import ExpensesFilter from "./newExpense/ExpensesFilter";
 
 function Expenses(props){
 
+
+
 const [filteredYear, setFilteredYear] = useState("2020"); 
+
+const filteredExpenses = props.items.filter(expenses =>{
+  return expenses.date.getFullYear().toString() === filteredYear;
+});
 
 
 function filterChangeHolder(selectedYear){
@@ -16,8 +22,9 @@ function filterChangeHolder(selectedYear){
     return(
         <Card className="expenses">
     <ExpensesFilter selected={filteredYear} onFilterChange={filterChangeHolder}></ExpensesFilter>
-    {props.items.map((expensesElement) => (
+    {filteredExpenses.map((expensesElement) => (
         <ExpenseItem
+        key={expensesElement.id}
           title={expensesElement.title}
           amount={expensesElement.amount}
           date={expensesElement.date}
